59dc328e732ad00538519f9bb6e7ff91ad0396cf
Sandeep <chsandeepreddy0405@gmail.com>
2024-02-24 16:55:27 +0530

    Initializing compiler

---
+++ phases/phase2/include/symbol_table.h
@@ -0,0 +1,84 @@
+/*
+#ifndef  STDIO_INCLUDE
+
+#define STDIO_INCLUDE
+#include <stdio.h>
+#include <string.h>
+
+#endif
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+extern int lineno;
+extern char error_string[];
+extern void yyerror(char*);
+#define max_entries 100
+int curr_entries = 0;
+
+struct symbol_table{
+    char* name;
+    int value;
+    struct symbol_table* next;
+};
+struct symbol_table* head = NULL;
+
+struct symbol_table* allot_entry(char* name){
+	struct symbol_table* curr = (struct symbol_table*)malloc(sizeof(struct symbol_table));
+	curr->name = strdup(name);
+	curr->value = 0;
+	curr->next = NULL;
+	return curr;
+}
+
+struct symbol_table* declare_variable(char* name){
+	if(head == NULL){
+		curr_entries++;
+		head = allot_entry(name);
+		return head;
+	}
+	struct symbol_table* prev = NULL;
+	struct symbol_table* trav = head; // traversal pointer
+    	
+    while(trav != NULL){
+		if(strcmp(trav->name, name) == 0){
+			sprintf(error_string, "Compilation error: Varaible '%s' is already declared", name);
+			yyerror(error_string);
+		}
+		prev = trav;
+		trav = trav->next;
+    }
+    if(curr_entries == max_entries){
+    	yyerror("Maximum limit crossed in symbol_table");
+    }
+    curr_entries++;
+    prev->next = allot_entry(name);
+    return prev->next;	
+	
+}	
+
+
+struct symbol_table* lookup(char* name){
+	if(head == NULL){
+		sprintf(error_string, "Compilation error: Varaible '%s' is not declared", name);
+		yyerror(error_string);
+	}
+	struct symbol_table* prev = NULL;	
+	struct symbol_table* trav = head; // traversal pointer
+    	
+    while(trav != NULL){
+		if(strcmp(trav->name, name) == 0){
+			return trav;
+		}
+		prev = trav;
+		trav = trav->next;
+    }
+
+     sprintf(error_string, "Compilation error: Varaible '%s' is not declared", name);
+     yyerror(error_string);
+}
+
